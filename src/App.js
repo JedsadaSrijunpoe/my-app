86b7e22a7476dac03aea51419c9bf51be93fea26
JedsadaSrijunpoe <tae456852@gmail.com>
2024-03-30 22:07:55 +0700

    Add div

--- src/App.js
+++ src/App.js
@@ -4,59 +4,62 @@ import { useState } from 'react';
 function App() {
   return (
     <div className="App">
-      <p className='topic'>สถานที่</p><br/><br/>
+      <div className='App-header'></div>
+      <br/>
+      <p className='topic'>สถานที่ ( 10 ) </p><br/><br/>
       <div className="image-row">
-        <GrayscaleImageWithOverlay src={"/place1.jpg"}/>
-        <GrayscaleImageWithOverlay src={"/place2.jpg"}/>
+        <LocationImage src={"/place1.jpg"}/>
+        <LocationImage src={"/place2.jpg"}/>
       </div>
       <div className="image-row">
-        <GrayscaleImageWithOverlay src={"/place3.jpg"}/>
-        <GrayscaleImageWithOverlay src={"/place4.jpg"}/>
+        <LocationImage src={"/place3.jpg"}/>
+        <LocationImage src={"/place4.jpg"}/>
       </div>
       <div className='image-row'>
-        <GrayscaleImageWithOverlay src={"/place5.jpg"}/>
-        <GrayscaleImageWithOverlay src={"/place6.jpg"}/>
+        <LocationImage src={"/place5.jpg"}/>
+        <LocationImage src={"/place6.jpg"}/>
       </div>
       <div className='image-row'>
-        <GrayscaleImageWithOverlay src={"/place7.jpg"}/>
-        <GrayscaleImageWithOverlay src={"/place8.jpg"}/>
+        <LocationImage src={"/place7.jpg"}/>
+        <LocationImage src={"/place8.jpg"}/>
       </div>
       <div className='image-row'>
-        <GrayscaleImageWithOverlay src={"/place9.jpg"}/>
-        <GrayscaleImageWithOverlay src={"/place10.jpg"}/>
+        <LocationImage src={"/place9.jpg"}/>
+        <LocationImage src={"/place10.jpg"}/>
       </div>
       
       <br/>
-      <p className='topic'>ผู้เล่น</p><br/><br/>
-      <PlayerDiv text={"ผู้เล่นที่ 1"} /><br/>
-      <PlayerDiv text={"ผู้เล่นที่ 2"} /><br/>
-      <PlayerDiv text={"ผู้เล่นที่ 3"} /><br/>
-      <PlayerDiv text={"ผู้เล่นที่ 4"} /><br/>
-      <PlayerDiv text={"ผู้เล่นที่ 5"} /><br/>
-      <PlayerDiv text={"ผู้เล่นที่ 6"} /><br/>
-      <PlayerDiv text={"ผู้เล่นที่ 7"} /><br/>
-      <br/>
+      <div className='player-container'>
+        <br/>
+        <p className='topic'>ผู้เล่น ( 7 )</p><br/><br/>
+        <PlayerDiv text={"ผู้เล่นที่ 1"} /><br/>
+        <PlayerDiv text={"ผู้เล่นที่ 2"} /><br/>
+        <PlayerDiv text={"ผู้เล่นที่ 3"} /><br/>
+        <PlayerDiv text={"ผู้เล่นที่ 4"} /><br/>
+        <PlayerDiv text={"ผู้เล่นที่ 5"} /><br/>
+        <PlayerDiv text={"ผู้เล่นที่ 6"} /><br/>
+        <PlayerDiv text={"ผู้เล่นที่ 7"} /><br/>
+        <br/>
+      </div>
 
+      <br/>
       <ResetButton />
     </div>
   );
 }
 
-function GrayscaleImageWithOverlay({src}) {
+function LocationImage({src}) {
   const [isGrayscale, setIsGrayscale] = useState(false);
   const resetState = () => {
     setIsGrayscale(false);
   };
-
   return (
     <div className='image-container'>
         <img
             src={src}
             alt="with Grayscale and Overlay"
             className={`my-image ${isGrayscale ? 'grayscale-image' : ''}`}
-            onClick={() => {
-                setIsGrayscale(!isGrayscale);
-            }}
+            onClick={() => setIsGrayscale(!isGrayscale)}
         />
     </div>
   );
